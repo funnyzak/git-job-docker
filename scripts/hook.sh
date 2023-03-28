c4a606bc4918070f6109f4c8e4d8adc5acc63fa7
Leon <silenceace@gmail.com>
2023-03-28 15:08:49 +0800

    add dingding md support to push message (#9)

--- scripts/hook.sh
+++ scripts/hook.sh
@@ -35,18 +35,24 @@ fi
 
 run_command "$AFTER_BUILD_COMMANDS" "after build"
 
+cd ${CODE_DIR}
+
 hook_end_time=$(date +%s)
 hook_elapsed_time=$((hook_end_time-hook_start_time))
 human_readable_time=$(date -d@${hook_elapsed_time} -u +%H:%M:%S)
-
-cd ${CODE_DIR}
+short_commit_id=$(echo $(prase_git_commitid) | cut -c 1-20)
 
 # push success message
-log "**Git branch**: $(parse_git_branch)
-**Git commit id**: $(prase_git_commitid) 
-**Git commit message**: $(parse_git_message)
-**Git commit autho**r: $(parse_git_author)
-**Git commit date**: $(parse_git_date)
-**Total elapsed time**: $human_readable_time" "info" "true"
+log "## Commit
+
+$(parse_git_message)
+
+* Branch: **$(parse_git_branch)**
+* Commit ID: **${short_commit_id}**
+* Author: **$(parse_git_author)**
+* Date: **$(parse_git_date)**
+
+## Elapsed
+* Elapsed Time: $human_readable_time" "info" "true"
 
 log "Run hook script done."
